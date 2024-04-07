7d39da2ccc05beb5b412dbb821ccf6b38ceed025
Daniele Olmisani <daniele.olmisani@gmail.com>
2024-04-07 08:49:22 +0200

    Update cf_file_test.rs

--- tests/cf_file_test.rs
+++ tests/cf_file_test.rs
@@ -1,10 +1,12 @@
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
+
     use scars::cf::file::{File, FileTrait};
 
     #[test]
     fn it_works() {
-        if let Ok(mut f) = File::open(&String::from("Cargo.toml"), "./") {
+        if let Ok(mut f) = File::open(&String::from("Cargo.toml"), Path::new("./")) {
             let data = &mut vec![0; 1024];
             let result = f.read(data);
             println!("{:?}", data);
@@ -17,7 +19,7 @@ mod tests {
     #[test]
     fn test_open_exception() {
         let n = String::from("xxxxx.xxx");
-        let r = File::open(&n);
+        let r = File::open(&n, Path::new("./"));
         match r {
             Ok(_) => todo!(),
             Err(e) => print!("{:?}", e),
